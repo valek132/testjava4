ccc6eb2cdf807cde3f6095926af8697070d1347f
Valek <pasport96@gmail.com>
2023-02-27 14:47:31 +0300

    first commit

---
+++ task1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+
+public class task1 {
+
+    public static ArrayDeque<Integer> getReversDeque(int count) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        ArrayDeque<Integer> reverseDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < count; i++) {
+            int rand = (int) (Math.random() * (100 - 1));
+            deque.addFirst(rand);
+        }
+        System.out.println(deque);
+
+        for (int i = 0; i < count; i++) {
+            reverseDeque.addFirst(deque.getFirst());
+            deque.removeFirst();
+        }
+        return reverseDeque;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getReversDeque(10));
+
+    }
+
+}
